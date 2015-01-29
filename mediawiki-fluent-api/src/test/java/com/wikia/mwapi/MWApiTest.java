b5c6dd585e1ab6f5ed11cc96bf8fde8565036b15
Damon Snyder <drsnyder@gmail.com>
2015-01-29 13:45:51 -0800

    Fix the MWApi test
    Only mock HttpClient.
    Use argument capture to confirm the URL construction.

--- mediawiki-fluent-api/src/test/java/com/wikia/mwapi/MWApiTest.java
+++ mediawiki-fluent-api/src/test/java/com/wikia/mwapi/MWApiTest.java
@@ -2,6 +2,12 @@ package com.wikia.mwapi;
 
 import com.wikia.mwapi.domain.ApiResponse;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import io.dropwizard.testing.FixtureHelpers;
 import org.junit.Test;
@@ -27,25 +33,37 @@ public class MWApiTest {
 
   @Test
   public void testGet() throws IOException {
-    MWApi mwApi = Mockito.mock(MWApi.class);
-    Mockito.when(mwApi.get()).thenCallRealMethod();
-    Mockito.when(mwApi.wikia(Mockito.anyString())).thenCallRealMethod();
-    Mockito.when(mwApi.queryAction()).thenCallRealMethod();
-    Mockito.when(mwApi.titles(Mockito.anyString())).thenCallRealMethod();
+    String wikia = "muppet";
+    String title = "Kermit the Frog";
+    String url = MWApi.createBuilder()
+        .wikia(wikia)
+        .queryAction()
+        .titles(title)
+        .url();
+    HttpUriRequest request = new HttpGet(url);
+
+    HttpClient httpClient = Mockito.mock(HttpClient.class);
+    HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
+    HttpEntity httpEntity = Mockito.mock(HttpEntity.class);
 
     InputStream jsonStream = new ByteArrayInputStream(
         FixtureHelpers.fixture("fixtures/kermit-the-frog-title.json").getBytes(StandardCharsets.UTF_8)
     );
-    Mockito.when(mwApi.handleMWRequest(Mockito.anyString())).thenReturn(jsonStream);
+    Mockito.when(httpEntity.getContent()).thenReturn(jsonStream);
+    Mockito.when(httpResponse.getEntity()).thenReturn(httpEntity);
+    Mockito.when(httpClient.execute(Mockito.any(HttpUriRequest.class))).thenReturn(httpResponse);
 
-    ApiResponse response = mwApi
-        .wikia("muppet")
+    ApiResponse response = MWApi.createBuilder(httpClient)
+        .wikia(wikia)
         .queryAction()
-        .titles("Kermit the Frog")
+        .titles(title)
         .get();
     assertEquals("Kermit the Frog", response.getQuery().getFirstPage().getTitle());
     assertEquals(50, response.getQuery().getFirstPage().getPageId());
-  }
 
+    ArgumentCaptor<HttpUriRequest> argument = ArgumentCaptor.forClass(HttpUriRequest.class);
+    Mockito.verify(httpClient).execute(argument.capture());
+    assertEquals(url, argument.getValue().getURI().toString());
+  }
 
 }
