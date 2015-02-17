0db692f1ce0a2f358ad966392836c86904b48e8f
Damian Jozwiak <damian@wikia-inc.com>
2015-02-17 13:33:14 +0100

    sirius initial commit

---
+++ sirius-example/src/main/java/com/wikia/exampleservice/ExampleServiceApplication.java
@@ -0,0 +1,52 @@
+package com.wikia.exampleservice;
+
+import com.theoryinpractise.halbuilder.jaxrs.JaxRsHalBuilderSupport;
+import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
+import com.wikia.exampleservice.configuration.ExampleConfiguration;
+import com.wikia.exampleservice.health.ExampleHealthCheck;
+import com.wikia.exampleservice.resources.ExampleResource;
+import com.wikia.pandora.core.consul.ConsulBundle;
+import com.wikia.pandora.core.consul.ConsulConfig;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ExampleServiceApplication extends Application<ExampleConfiguration> {
+
+  public static void main(String[] args) throws Exception {
+    new ExampleServiceApplication().run(args);
+  }
+
+  @Override
+  public String getName() {
+    return "pandora-example"; // no uppercase letters
+  }
+
+
+  @Override
+  public void initialize(Bootstrap<ExampleConfiguration> bootstrap) {
+//    bootstrap.addBundle(new ConsulBundle<ExampleConfiguration>() {
+//      @Override
+//      protected ConsulConfig narrowConfig(ExampleConfiguration config) {
+//        return config.getConsulConfig();
+//      }
+//    });
+  }
+
+  @Override
+  public void run(ExampleConfiguration configuration, Environment environment) throws Exception {
+
+    //register healthCheck (mandatory)
+    final ExampleHealthCheck healthCheck = new ExampleHealthCheck();
+    environment.healthChecks().register("SimpleHealthCheck", healthCheck);
+
+    StandardRepresentationFactory representationFactory = new StandardRepresentationFactory();
+    ExampleResource exampleResource = new ExampleResource(representationFactory);
+    exampleResource.setGreetingsWord(configuration.getGreetingsWord());
+    environment.jersey().register(exampleResource);
+
+    //Optional
+    environment.jersey().register(JaxRsHalBuilderSupport.class);
+  }
+}
