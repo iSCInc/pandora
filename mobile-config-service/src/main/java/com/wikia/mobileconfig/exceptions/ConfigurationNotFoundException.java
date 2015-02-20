3fe2462b0f1725d3e257029a8e3e2ce3d615d24d
nAndy <nandy@wikia-inc.com>
2015-02-19 16:28:20 -0800

    addressed sonar issue about exception classes being immutable

--- mobile-config-service/src/main/java/com/wikia/mobileconfig/exceptions/ConfigurationNotFoundException.java
+++ mobile-config-service/src/main/java/com/wikia/mobileconfig/exceptions/ConfigurationNotFoundException.java
@@ -5,7 +5,7 @@ import com.wikia.mobileconfig.core.Problem;
 import javax.ws.rs.core.Response;
 
 public class ConfigurationNotFoundException extends MobileConfigException {
-  static String EXCEPTION_MESSAGE_FORMAT = "Configuration for %s on %s could not be found";
+  static final String EXCEPTION_MESSAGE_FORMAT = "Configuration for %s on %s could not be found";
 
   public ConfigurationNotFoundException(String platform, String appTag) {
     super(
