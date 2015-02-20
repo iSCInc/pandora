1fcbaa77ec18458a4ff0f4a0eca65ad84e023d96
nAndy <nandy@wikia-inc.com>
2015-02-19 17:03:10 -0800

    refactored controller so it throws only checked exceptions

--- mobile-config-service/src/main/java/com/wikia/mobileconfig/resources/MobileConfigResource.java
+++ mobile-config-service/src/main/java/com/wikia/mobileconfig/resources/MobileConfigResource.java
@@ -6,6 +6,7 @@ import com.theoryinpractise.halbuilder.api.Representation;
 import com.theoryinpractise.halbuilder.api.RepresentationFactory;
 
 import com.wikia.mobileconfig.MobileConfigApplication;
+import com.wikia.mobileconfig.core.EmptyMobileConfiguration;
 import com.wikia.mobileconfig.core.MobileConfiguration;
 import com.wikia.mobileconfig.exceptions.ConfigurationNotFoundException;
 import com.wikia.mobileconfig.exceptions.InvalidApplicationTagException;
@@ -13,11 +14,19 @@ import com.wikia.mobileconfig.exceptions.MobileConfigException;
 import com.wikia.mobileconfig.service.ConfigurationService;
 import com.wikia.mobileconfig.gateway.AppsListService;
 
+import java.io.IOException;
+
 import javax.ws.rs.*;
 
 @Path("/configurations/platform/{platform}/app/{app-tag}")
 @Produces(RepresentationFactory.HAL_JSON)
 public class MobileConfigResource {
+
+  private static String APP_TAG_VALIDATION_EXCEPTION_MESSAGE =
+      "An exception occurred while validating application tag";
+
+  private static String GET_CONFIGURATION_EXCEPTION_MESSAGE =
+      "An exception occurred while getting configuration";
 
   private final ConfigurationService appConfiguration;
   private final AppsListService appsList;
@@ -39,14 +48,14 @@ public class MobileConfigResource {
       @PathParam("app-tag") String appTag,
       @QueryParam("ui-lang") String uiLang,
       @QueryParam("content-lang") String contentLang
-  ) throws java.io.IOException, MobileConfigException {
+  ) throws MobileConfigException {
+    Boolean isValidAppTag = isValidAppTag(platform, appTag);
 
-    if (!this.appsList.isValidAppTag(platform, appTag)) {
+    if (!isValidAppTag) {
       throw new InvalidApplicationTagException(appTag);
     }
 
-    MobileConfiguration configuration =
-        this.appConfiguration.getConfiguration(platform, appTag, uiLang, contentLang);
+    MobileConfiguration configuration = getConfiguration(platform, appTag, uiLang, contentLang);
 
     if (configuration.getModules().isEmpty()) {
       throw new ConfigurationNotFoundException(platform, appTag);
@@ -57,5 +66,35 @@ public class MobileConfigResource {
     ).withBean(configuration);
 
     return rep;
+  }
+
+  private Boolean isValidAppTag(String platform, String appTag) {
+    Boolean isValidAppTag;
+
+    try {
+      isValidAppTag = this.appsList.isValidAppTag(platform, appTag);
+    } catch (IOException e) {
+      isValidAppTag = false;
+      MobileConfigApplication.LOGGER.info(APP_TAG_VALIDATION_EXCEPTION_MESSAGE, e);
+    }
+
+    return isValidAppTag;
+  }
+
+  private MobileConfiguration getConfiguration(String platform,
+                                               String appTag,
+                                               String uiLang,
+                                               String contentLang)
+  {
+    MobileConfiguration configuration;
+
+    try {
+      configuration = this.appConfiguration.getConfiguration(platform, appTag, uiLang, contentLang);
+    } catch (IOException e) {
+      configuration = new EmptyMobileConfiguration();
+      MobileConfigApplication.LOGGER.info(GET_CONFIGURATION_EXCEPTION_MESSAGE, e);
+    }
+
+    return configuration;
   }
 }
