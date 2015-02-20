42363d8c44827db14b951394e803837ca975236b
nAndy <nandy@wikia-inc.com>
2015-02-19 17:04:31 -0800

    changed accessibility of class a class field

--- mobile-config-service/src/main/java/com/wikia/mobileconfig/exceptions/MobileConfigException.java
+++ mobile-config-service/src/main/java/com/wikia/mobileconfig/exceptions/MobileConfigException.java
@@ -4,7 +4,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
 public abstract class MobileConfigException extends WebApplicationException {
-  public static final String RESPONSE_TYPE = "application/problem+json";
+  protected static final String RESPONSE_TYPE = "application/problem+json";
 
   public MobileConfigException(Response exceptionResponse) {
     super(exceptionResponse);
