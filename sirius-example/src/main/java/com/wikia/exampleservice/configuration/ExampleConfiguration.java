76c8b0f1b8f7ed45177b967db50c04a19be82599
rafal.kalinski <kalina@wikia-inc.com>
2015-03-04 20:38:44 +0100

    SOC-299 restoring original exampleservice

---
+++ sirius-example/src/main/java/com/wikia/exampleservice/configuration/ExampleConfiguration.java
@@ -0,0 +1,25 @@
+package com.wikia.exampleservice.configuration;
+
+import com.wikia.pandora.core.consul.ConsulConfig;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import io.dropwizard.Configuration;
+
+public class ExampleConfiguration extends Configuration {
+
+  @NotEmpty
+  @NotNull
+  private String greetingsWord; // value is set up by dropwizard reflection magic.
+  private ConsulConfig consulConfig;
+
+  public String getGreetingsWord() {
+    return greetingsWord;
+  }
+
+  public ConsulConfig getConsulConfig() {
+    return consulConfig;
+  }
+}
