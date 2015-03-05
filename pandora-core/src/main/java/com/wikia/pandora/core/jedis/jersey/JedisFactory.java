8ffac573ac3226b6e528735c588b2a52dbe15f87
rafal.kalinski <kalina@wikia-inc.com>
2015-03-05 12:58:27 +0100

    SOC-299 adding some space into jersey JedisFactory code

--- pandora-core/src/main/java/com/wikia/pandora/core/jedis/jersey/JedisFactory.java
+++ pandora-core/src/main/java/com/wikia/pandora/core/jedis/jersey/JedisFactory.java
@@ -9,13 +9,16 @@ import redis.clients.jedis.JedisPool;
  */
 public class JedisFactory implements Factory<Jedis> {
     private final JedisPool pool;
+
     public JedisFactory(JedisPool pool) {
         this.pool = pool;
     }
+
     @Override
     public Jedis provide() {
         return pool.getResource();
     }
+
     @Override
     public void dispose(Jedis jedis) {
 // we can close the connection directly because Jedis knows
