0f08ab25cc2f735df13483739be876cc873a7f42
Pawel Chojnacki <pawel@chojnacki.ws>
2015-03-09 21:32:39 +0100

    move influx metrics reporter to its own subproject

--- lib/dropwizard/metrics-influx/src/main/java/com/wikia/dropwizard/metrics/influx/InfluxDbReporterFactory.java
+++ lib/dropwizard/metrics-influx/src/main/java/com/wikia/dropwizard/metrics/influx/InfluxDbReporterFactory.java
@@ -1,4 +1,4 @@
-package com.wikia.pandora.core.metrics;
+package com.wikia.dropwizard.metrics.influx;
 
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
--- lib/dropwizard/metrics-influx/src/main/java/com/wikia/dropwizard/metrics/influx/RegexFilter.java
+++ lib/dropwizard/metrics-influx/src/main/java/com/wikia/dropwizard/metrics/influx/RegexFilter.java
@@ -1,4 +1,4 @@
-package com.wikia.pandora.core.metrics;
+package com.wikia.dropwizard.metrics.influx;
 
 import com.google.common.collect.ImmutableSet;
 
