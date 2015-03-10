38a8db85deeda70889c6b52bdffe31ba931b148b
Mateusz Hołowiecki <Mateusz.Holowiecki@wikia-inc.com>
2015-03-10 17:30:39 +0100

    fix after merge

--- service/mobile-config/src/main/java/com/wikia/mobileconfig/MobileConfigModule.java
+++ service/mobile-config/src/main/java/com/wikia/mobileconfig/MobileConfigModule.java
@@ -1,10 +1,17 @@
 package com.wikia.mobileconfig;
 
+import com.wikia.dropwizard.consul.bundle.ConsulConfiguration;
+import com.wikia.dropwizard.consul.bundle.ConsulModule;
+import com.wikia.dropwizard.consul.bundle.ProvidesConsulConfiguration;
+import com.wikia.mobileconfig.service.application.AppsDeployerListContainer;
+import com.wikia.mobileconfig.service.application.AppsListService;
+import com.wikia.mobileconfig.service.application.CephAppsListService;
+import com.wikia.mobileconfig.service.configuration.CephConfigurationService;
+import com.wikia.mobileconfig.service.configuration.ConfigurationService;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.Stage;
 import com.google.inject.name.Named;
 import com.netflix.governator.guice.lazy.LazySingleton;
 import io.dropwizard.client.HttpClientBuilder;
@@ -12,16 +19,6 @@ import io.dropwizard.setup.Environment;
 import org.apache.http.client.HttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.wikia.mobileconfig.health.AppsDeployerHealthCheck;
-import com.wikia.mobileconfig.service.application.AppsDeployerListContainer;
-import com.wikia.mobileconfig.service.application.AppsListService;
-import com.wikia.mobileconfig.service.application.CephAppsListService;
-import com.wikia.mobileconfig.service.configuration.CephConfigurationService;
-import com.wikia.mobileconfig.service.configuration.ConfigurationService;
-import com.wikia.pandora.core.consul.ConsulConfiguration;
-import com.wikia.dropwizard.consul.bundle.ConsulModule;
-import com.wikia.dropwizard.consul.bundle.ProvidesConsulConfiguration;
 
 
 public class MobileConfigModule extends AbstractModule {
