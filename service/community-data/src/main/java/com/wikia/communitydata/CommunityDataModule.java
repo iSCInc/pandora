f676d68036e13bb5f6417182c93fbf45b731b2f5
nmonterroso <nelson@wikia-inc.com>
2015-03-12 16:07:59 -0700

    removing representationfactory stuff

--- service/community-data/src/main/java/com/wikia/communitydata/CommunityDataModule.java
+++ service/community-data/src/main/java/com/wikia/communitydata/CommunityDataModule.java
@@ -4,19 +4,11 @@ import com.wikia.communitydata.configuration.CommunityDataConfiguration;
 import com.wikia.dropwizard.consul.bundle.ProvidesConsulConfiguration;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.theoryinpractise.halbuilder.api.RepresentationFactory;
-import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
 
 public class CommunityDataModule extends AbstractModule {
 
   @Override
   protected void configure() {
     bind(ProvidesConsulConfiguration.class).to(CommunityDataConfiguration.class);
-  }
-
-  @Provides
-  public RepresentationFactory getRepresentationFactory() {
-    return new StandardRepresentationFactory();
   }
 }
