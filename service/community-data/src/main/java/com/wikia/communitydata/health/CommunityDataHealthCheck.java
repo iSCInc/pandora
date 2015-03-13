95e789246707090a3fa87152c9b646b9d539c94f
nmonterroso <nelson@wikia-inc.com>
2015-03-13 11:50:46 -0700

    renaming to simple-check

--- service/community-data/src/main/java/com/wikia/communitydata/health/CommunityDataHealthCheck.java
+++ service/community-data/src/main/java/com/wikia/communitydata/health/CommunityDataHealthCheck.java
@@ -1,9 +1,9 @@
 package com.wikia.communitydata.health;
 
-import com.codahale.metrics.health.HealthCheck;
 import com.hubspot.dropwizard.guice.InjectableHealthCheck;
 
 public class CommunityDataHealthCheck extends InjectableHealthCheck {
+
   @Override
   public String getName() {
     return "simple-check";
